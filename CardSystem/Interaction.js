8d61d8a9345e7c830d72995962c8dabc5f16ece4
C.Maureder <maureder.christoph@gmail.com>
2024-12-05 09:04:15 +0100

    Change Path

--- CardSystem/Interaction.js
+++ CardSystem/Interaction.js
@@ -60,7 +60,7 @@ function draw() {
 }
 
 function loadCardCategories(callback) {
-    loadXML('../Cards/cardCategories.xml', (xml) => {
+    loadXML('../../Cards/cardCategories.xml', (xml) => {
         if (!xml) {
             console.error("Failed to load XML file for categories.");
             return;
@@ -97,7 +97,7 @@ function loadCardCategories(callback) {
 }
 
 function loadCards(callback) {
-    loadXML('../Cards/cards.xml', (xml) => {
+    loadXML('../../Cards/cards.xml', (xml) => {
         if (!xml) {
             console.error("Failed to load XML file.");
             return;
